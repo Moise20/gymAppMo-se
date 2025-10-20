0bce2fe852459b051ba5d15c610d4affb0e043b2
PANA <panaessognim@gmail.com>
2025-10-20 16:32:22 +0200

    feat(equipment): list + card

---
+++ src/app/features/equipment/equipment-card/equipment-card.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Card } from '../../../shared/ui/card/card';
+export interface Equipment { title:string; img:string; }
+@Component({
+  selector: 'app-equipment-card',
+  standalone: true,
+  imports: [Card],
+  templateUrl: './equipment-card.html',
+  styleUrl: './equipment-card.css'
+})
+export class EquipmentCard {
+@Input() item?: Equipment;
+}
---
+++ src/app/features/equipment/equipment-list/equipment-list.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { EquipmentCard } from '../equipment-card/equipment-card';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-equipment-list',
+   standalone: true,
+  imports: [NgFor, EquipmentCard],
+  templateUrl: './equipment-list.html',
+  styleUrl: './equipment-list.css'
+})
+export class EquipmentList {
+data: Equipment[] = [
+    { title:'Haltères & racks', img:'/assets/eq1.jpg' },
+    { title:'Cardio & vélos', img:'/assets/eq2.jpg' },
+    { title:'Machines guidées', img:'/assets/eq3.jpg' },
+    { title:'Box CrossFit', img:'/assets/eq4.jpg' },
+    { title:'Zone stretching', img:'/assets/eq5.jpg' },
+    { title:'Poids libres', img:'/assets/eq6.jpg' },
+  ];
+}
