86730ffa583c428487825c03e0a6ea0f3fcdb4f6
PANA <panaessognim@gmail.com>
2025-10-21 16:55:02 +0200

    correction du composant hero-section pour utiliser l'image dans le html

--- src/app/features/home/hero-section/hero-section.ts
+++ src/app/features/home/hero-section/hero-section.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-<<<<<<< Updated upstream
 import { RouterLink } from '@angular/router';
 import { Button } from '../../../shared/ui/button/button';
-=======
-import heroImage from '../../../assets/hero.png';
 
->>>>>>> Stashed changes
+
+
 
 @Component({
   selector: 'app-hero-section',
@@ -15,5 +13,5 @@ import heroImage from '../../../assets/hero.png';
   styleUrl: './hero-section.css'
 })
 export class HeroSection {
- heroImage = heroImage;
+
 }
